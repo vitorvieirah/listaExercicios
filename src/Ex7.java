e0fa35e7c84849521711c070c109a53f7fd84196
VitorVieira <vitorhvvieira@gmail.com>
2023-05-22 14:04:29 -0300

    começando ex 7

--- src/Ex7.java
+++ src/Ex7.java
@@ -1,6 +1,12 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Ex7 {
-    public static void main(String[] args) {
-        System.out.println("oi");
-        System.out.println("ta dando certoo");
+        //Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double numero = sc.nextDouble();
+            DecimalFormat formato = new DecimalFormat("#");
+            System.out.println(formato.format(numero));
     }
 }
