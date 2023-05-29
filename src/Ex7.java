dcefe21f3ac1391f9bfe9d7ec51c2950275f6cac
VitorVieira <vitorhvvieira@gmail.com>
2023-05-29 13:50:25 -0300

    terminando exercicio 7

--- src/Ex7.java
+++ src/Ex7.java
@@ -1,12 +1,25 @@
+import java.sql.BatchUpdateException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ex7 {
         //Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
-        public static void main(String[] args) {
+
+
+        public static boolean validarNumero(double numero){
+            String numComp = String.valueOf(numero);
+            return numComp.contains(".0");
+        }
+        public static double menu(){
             Scanner sc = new Scanner(System.in);
-            double numero = sc.nextDouble();
-            DecimalFormat formato = new DecimalFormat("#");
-           
+            System.out.println("Digite o numero que deseja comparar:");
+            return sc.nextDouble();
+        }
+        public static void main(String[] args) {
+            if(validarNumero(menu())){
+                System.out.println("O nuemro não é decimal");
+            }else{
+                System.out.println("O numero é decimal");
+            }
     }
 }
