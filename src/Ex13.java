cf9ae34dd19bb53cd544c0c80037857c63d9a429
VitorVieira <vitorhvvieira@gmail.com>
2023-06-23 09:47:49 -0300

    terminei ex13 - começando ex14

--- src/Ex13.java
+++ src/Ex13.java
@@ -4,43 +4,23 @@ import java.time.LocalDate;
 public class Ex13 {
     //3. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas em dias.
 
-    public static int menuInicial(){
+    public static String menuInicial(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("DIGITE A SUA IDADE DA SEQUINTE FORMA: ANOS, MESES. DIAS");
+        System.out.println("DIGITE A SUA IDADE DA SEQUINTE FORMA: ANOS, MESES, DIAS");
             int anos = sc.nextInt();
             int meses = sc.nextInt();
             int dias = sc.nextInt();
-            return anos + meses + dias;
+            return calculoIdadeDias(anos, meses, dias);
     }
 
     public static String calculoIdadeDias(int anos, int meses, int dias){
         int diasTotais = anos * 365;
-
+        diasTotais += meses * 30;
+        diasTotais += dias;
+        return "OS DIAS TOTAIS SÃO: " + diasTotais;
     }
 
-    public static int validaMes(int meses){
-        LocalDate dataAtual = LocalDate.now();
-        //int ano = dataAtual.getYear();
-        int mes = dataAtual.getMonthValue();
-        //int dia = dataAtual.getDayOfMonth();
-        int[] mesvalidacao = new int[12];
-        mesvalidacao[0] = 1;
-        mesvalidacao[1] = 2;
-        mesvalidacao[2] = 3;
-        mesvalidacao[3] = 4;
-        mesvalidacao[4] = 5;
-        mesvalidacao[5] = 6;
-        mesvalidacao[6] = 7;
-        mesvalidacao[7] = 8;
-        mesvalidacao[8] = 9;
-        mesvalidacao[9] = 10;
-        mesvalidacao[10] = 11;
-        mesvalidacao[11] = 12;
-        for (int i = mes - 1; i < meses; i ++){
-
-        }
-    }
     public static void main(String[] args) {
-
+        System.out.println(menuInicial());
     }
 }
