afc8a4063769dae42ff6f8677a84f890903ae11d
VitorVieira <vitorhvvieira@gmail.com>
2023-07-12 19:02:34 -0300

    MELHORANDO CONDIÇÕES

--- src/Ex22/CriancaRepository.java
+++ src/Ex22/CriancaRepository.java
@@ -6,41 +6,40 @@ import java.util.List;
 public abstract class CriancaRepository {
     static List<Crianca> criancas = new ArrayList<>();
 
-    public static void addCriancas(Crianca crianca){
+    public static void addCriancas(Crianca crianca) {
         criancas.add(crianca);
     }
 
-    public static int getPorcentagemMortas(){
+    public static int getPorcentagemMortas() {
         int total = criancas.size();
         int mortes = 0;
         for (int i = 0; i < total; i++) {
-           if(criancas.get(i).getViva()){
-           }else {
-               mortes += 1;
-           }
+            if (!criancas.get(i).getViva()) {
+                mortes += 1;
+            }
         }
         return total / (mortes * 100);
     }
 
-    public static int getPorcetagemMasculino(){
+    public static int getPorcetagemMasculino() {
         int total = criancas.size();
         int porcetagem = 0;
-        for (int i = 0; i < total; i ++) {
-            if(criancas.get(i).getViva()){
-            }else if(criancas.get(i).getSexo().equalsIgnoreCase("masculino")){
+        for (int i = 0; i < total; i++) {
+            if (!criancas.get(i).getViva() && criancas.get(i).getSexo().equalsIgnoreCase("masculino")) {
                 porcetagem += 1;
             }
         }
-        return total /(porcetagem * 100);
+        return total / (porcetagem * 100);
     }
 
-    public static int getCriancasIdade(){
+    public static int getCriancasIdade() {
         int total = 0;
         for (int i = 0; i < criancas.size(); i++) {
-            if(criancas.get(i).getIdade() <= 24){
+            if (criancas.get(i).getIdade() <= 24) {
                 total += 1;
             }
         }
         return total;
     }
 }
+
