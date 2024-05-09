981215e2263f6ef16f12dd489fddb8e1a936fdc8
VitorVieira <vitorhvvieira@gmail.com>
2024-05-09 16:03:58 -0300

    Continuando com o exercício 36

--- src/Ex36.java
+++ src/Ex36.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Ex36 {
     /*O jogo de senha é um jogo da web onde você escreverá uma senha como ela diz, mas tem uma diferença: você tem que seguir algumas regras incrivelmente estúpidas para completar a senha perfeita.
 
@@ -9,10 +11,10 @@ public class Ex36 {
     Saída
     Um booleano que indica se a senha é forte o suficiente para passar no jogo
     Regras
-    O comprimento máximo da senha é de 50 caracteres c
-    O comprimento mínimo da senha é de 20 caracteres c
-    O comprimento da senha deve ser um número primo c
-    Deve ter pelo menos 1 letra minúscula, 3 letras maiúsculas e 1 caractere especial
+    O comprimento máximo da senha é de 50 caracteres ✅
+    O comprimento mínimo da senha é de 20 caracteres ✅
+    O comprimento da senha deve ser um número primo ✅
+    Deve ter pelo menos 1 letra minúscula, 3 letras maiúsculas e 1 caractere especial ✅
     A soma de cada um dos dígitos da senha deve ser de no mínimo 25
     Deve conter o diminutivo de um mês
     Deve conter a data de hoje
@@ -24,6 +26,8 @@ public class Ex36 {
     A data dentro da senha terá o seguinte formato aaaaMMdd
     Os números de data contam como dígitos normais para a adição de todos
     Os caracteres especiais para este kata são [-._@#$&]*/
+
+    private static final char [] caractersEspeciais = {'-', '.', '_', '@', '#', '$', '&'};
 
     public static void main(String[] args) {
 
@@ -38,6 +42,7 @@ public class Ex36 {
         return senha.length() <= 50 && senha.length() >= 20;
     }
 
+    //Ver se não da pra usar recursividade
     private static boolean validaTamanhoPrimo(String senha){
         int num = senha.length();
         if(num > 2){
@@ -46,11 +51,32 @@ public class Ex36 {
                     return false;
             }
         }
-
         return true;
     }
 
     private static boolean validaLetras(String senha){
+        char [] senhaChars = senha.toCharArray();
+        boolean minuscula = false;
+        boolean caracterEspecial = false;
+        int contMaiusc = 0;
 
+        for (int i = 0; i < senhaChars.length; i++) {
+            if(senhaChars[i] == Character.toLowerCase(senhaChars[i])) {
+                minuscula = true;
+            }
+
+            if(senhaChars[i] == Character.toUpperCase(senhaChars[i])) {
+                contMaiusc ++;
+            }
+
+            for (int j = 0; j < caractersEspeciais.length; j++) {
+                if(senhaChars[i] == caractersEspeciais[i]){
+                    caracterEspecial = true;
+                    break;
+                }
+            }
+        }
+
+        return minuscula && caracterEspecial && contMaiusc == 3;
     }
 }
