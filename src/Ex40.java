19cf87829f5da146fbfb5297586602d438dd3fab
vitorv1 <vitorhvvieira@gmail.com>
2024-07-23 09:38:21 -0300

    Subindo alterações restantes

--- src/Ex40.java
+++ src/Ex40.java
@@ -13,18 +13,35 @@ Exemplos
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Ex40 {
+    public static void main(String[] args) {
+        findIt(new int[]{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5});
+    }
+
     public static int findIt(int[] a) {
         List<Integer> numbers = new ArrayList<>();
         for(Integer i : a){
             numbers.add(i);
         }
 
-        Stream<Integer> numbersSorted = numbers.stream().sorted();
+        numbers.sort(null);
+        int cont = 1;
+        int i;
+        for (i = 0; i < numbers.size() - 1; i++) {
+            if(Objects.equals(numbers.get(i), numbers.get(i + 1))){
+                cont++;
+            }else{
+                if(cont % 2 == 1){
+                    return numbers.get(i);
+                }else{
+                    cont = 1;
+                }
+            }
+        }
 
-        System.out.println(numbersSorted);
-        return null;
+        return numbers.get(i);
     }
 }
